928e0921fa9d9d4415aaa0fef9e41db003e2b28e
jumailtaj <jumailtajhidhayath2005@gmail.com>
2026-03-26 21:11:56 +0530

    new change

--- frontend/src/app/pages/AdminLoginPage.jsx
+++ frontend/src/app/pages/AdminLoginPage.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
 import { useAuth } from '../context/AuthContext';
 import { Button } from '../components/ui/button';
 import { Input } from '../components/ui/input';
