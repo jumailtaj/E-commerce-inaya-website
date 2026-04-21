63adb8a5ce4281ab6303efd33463c0fad873fbc0
jumailtaj <jumailtajhidhayath2005@gmail.com>
2026-04-21 16:57:36 +0530

    new

--- frontend/src/app/pages/SignupPage.jsx
+++ frontend/src/app/pages/SignupPage.jsx
@@ -122,7 +122,7 @@ export function SignupPage() {
                 </button>
               </div>
             </div>
-            
+
             <div>
               <label className="block text-sm font-medium text-gray-700">
                 Confirm Password
