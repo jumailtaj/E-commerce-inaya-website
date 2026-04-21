e4856dd17b88e33e2024d3173a11bb47c45f5d10
jumailtaj <jumailtajhidhayath2005@gmail.com>
2026-04-21 17:43:56 +0530

    analytics

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -5,6 +5,7 @@ import { AuthProvider } from './context/AuthContext';
 import { Toaster } from './components/ui/sonner';
 import { HelmetProvider } from 'react-helmet-async';
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
           <RouterProvider router={router} />
           <Toaster position="top-right" />
           <SpeedInsights />
+          <Analytics />
         </CartProvider>
       </AuthProvider>
     </HelmetProvider>
