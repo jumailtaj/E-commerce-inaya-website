0337a9abd1294c620e14f047c2312b615b34ca68
jumailtaj <jumailtajhidhayath2005@gmail.com>
2026-06-03 20:46:46 +0530

    feat: configure Vite with Tailwind CSS, path aliasing, and custom rollup chunks

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,23 +2,11 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    Sitemap({
-      hostname: 'https://www.inayaastore.in',
-      dynamicRoutes: [
-        '/cart',
-        '/privacy-policy',
-        '/terms-and-conditions',
-        '/refund-policy',
-        '/shipping-policy',
-      ],
-      exclude: ['/google8fc9e830a0d7c7eb'],
-    }),
   ],
   resolve: {
     alias: {
